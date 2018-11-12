2409bdac6d2e5c7852a983766dcf92c01ec5c66f
Tatsumizuki543z1 <38421209+Tatsumizuki543z1@users.noreply.github.com>
2018-11-12 17:58:19 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -5,9 +5,9 @@ var answer = parseInt(window.prompt('数当てゲーム。0~2の数字を入力�
 var message;
 if(answer === number){
   message = 'あたり！';
-}else if(answer < number){
+}else if (answer < number){
   message = '残念！もっと大きいです';
-}else if(number < answer){
+}else if (number < answer){
   message = '残念！もっと小さいです';
 }else{
   message = '0~3の数字を入力してください';
